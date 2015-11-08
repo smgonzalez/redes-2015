ed5191eba90e3a0d2ae251d61d393d1b886bcb47
segonzalez <segonzalez@baufest.com>
2015-11-08 17:50:12 -0300

    ignorar el anterior. este es el que queria subir :P

--- TP2/tp2.py
+++ TP2/tp2.py
@@ -4,52 +4,57 @@ import sys
 
 #traceroute sobre ICMP
 
-#Sea h la IP del host destino y sea ttl = 1.
-ip = sys.argv[2]
-ttl = 1
-
-#Repetir los siguientes pasos hasta obtener una respuesta ICMP de tipo
-#Echo Reply por parte de h:
-
 ECHO_REPLY = 0
 ECHO_REQUEST = 11
 
-TIMEOUT = 3
+TIMEOUT = 2
 MAX_TTL = 100
 REPEAT_COUNT = 3
 
 text_file = open("Output.txt", "w")
-text_file.write("TTL\tIntentos\tIp\tRTT Promedio\n")
+header = "TTL\tIntentos\tIp\tRTT Promedio"
+
+print(header)
+print(header, file=text_file)
 
 class Route:
 
 	def __init__(self):
-		self.hops = []
+		self.hops = dict()
 		
 	def trace(self, hostname):
 
+		#Sea h la IP del host destino y sea ttl = 1.
+
 		ttl = 1
+
+		#Repetir los siguientes pasos hasta obtener una respuesta ICMP de tipo
+		#Echo Reply por parte de h:
+		
 		reply = False
 		
-		while(!reply and ttl < MAX_TTL): #"Echo Reply"
+		while((not reply) and ttl < MAX_TTL):
 		
 			#Enviar un paquete ICMP de tipo Echo Request al host h cuyo campo
 			#TTL en el header IP valga ttl.
-			packet = IP(dst=ip, ttl=ttl) / ICMP()
+			packet = IP(dst=hostname, ttl=ttl) / ICMP()
 
 			rtt_sum = 0.0       # Suma de los RTT para cada TTL
 			cant_success = 0    # Cantidad de paquetes enviados y recibidos exitosamente para cada TTL
 			
-			attempt=0
-			while attempt <= REPEAT_COUNT):
+			attempt=1
+			while attempt <= REPEAT_COUNT:
 
 				rtt = int(round(time.time() * 1000))
-				answer = sr1(packet, TIMEOUT=TIMEOUT, verbose=0)
+				answer = sr1(packet, timeout=TIMEOUT, verbose=0)
 				rtt = int(round(time.time() * 1000)) - rtt
 				answer_ip = None
 
 				if answer != None:
 
+					if answer.type == ECHO_REPLY:
+						reply = True
+				
 					if answer_ip == None: 
 						ip_src = answer[IP].src
 					
@@ -61,10 +66,7 @@ class Route:
 					cant_success += 1
 				
 				attempt += 1
-							
-			text_file.write(str(ttl))
-			text_file.write("\t")
-			
+										
 			#Si se recibe una respuesta ICMP de tipo Time Exceeded, anotar la IP
 			#origen de dicho paquete.    
 			if rtt_sum > 0:
@@ -72,20 +74,17 @@ class Route:
 				rtt_prom = rtt_sum / cant_success
 				self.hops[ttl] = (cant_success, ip_src, rtt_prom)		# MAP: TTL -> (intentos, ip que respondio, promedio de RTT)
 				
-				for attempts, (ip, rtt_prom) in hosts.items():
-			
-					info_hop = "%d:\t%d\t%s\t%f.2ms\n" (ttl, attempts, ip, rtt_prom)
+				info_hop = "%d:\t%d\t%s\t%fms\n" % (ttl, cant_success, ip_src, rtt_prom)
 					
-					print(info_hop)
-					text_file.write(info_hop)
+				print(info_hop)
+				print(info_hop, file=text_file)
 
 			else:
 				#En otro caso, marcar como desconocido (*) el
 				#hop.
-				print("No hubo respuesta")
-				text_file.write("*************************\n")
-
-			print ("**********************")
+				noResponse = str(ttl) + ":\t **** No hubo respuesta ***"
+				print(noResponse)
+				print(noResponse, file=text_file)
 
 			#Incrementar ttl.
 			ttl = ttl+1
@@ -95,8 +94,8 @@ class Route:
 		
 def main(argv=sys.argv):
 	route = Route()
+	route.trace(argv[1])
 
-	route.trace([argv[1])
 	
 	
 if __name__ == '__main__':
